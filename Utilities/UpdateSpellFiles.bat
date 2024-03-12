fbd8b5e7bfa5614a1428ab21f39ce5f46f9d7c6d
Adam Liverman <livermana@gmail.com>
2024-03-11 22:43:35 -0400

    updated spell file downloader for laz

--- Utilities/UpdateSpellFiles.bat
+++ Utilities/UpdateSpellFiles.bat
@@ -1,3 +1,8 @@
-powershell -Command "Invoke-WebRequest http://192.99.254.193:3000/download/spells -OutFile '.\spells_us.zip'"
-powershell Expand-Archive '.\spells_us.zip' -DestinationPath 'G:\EQ\Project Lazarus' -force
-powershell Expand-Archive '.\spells_us.zip' -DestinationPath 'G:\EQ\Project_Lazarus_Bots' -force
+powershell -Command "Invoke-WebRequest http://192.99.254.193:3000/api/v1/eqemuserver/export-client-file/spells -OutFile '.\spells_us.txt'"
+powershell -Command "Invoke-WebRequest http://192.99.254.193:3000/api/v1/eqemuserver/export-client-file/dbstring -OutFile '.\dbstr_us.txt'"
+
+powershell Copy-Item '.\spells_us.txt' -Destination 'C:\Project Lazarus\RoF2\' -force
+powershell Copy-Item '.\spells_us.txt' -Destination 'C:\Project Lazarus\RoF3\' -force
+
+powershell Copy-Item '.\dbstr_us.txt' -Destination 'C:\Project Lazarus\RoF2\' -force
+powershell Copy-Item '.\dbstr_us.txt' -Destination 'C:\Project Lazarus\RoF3\' -force
